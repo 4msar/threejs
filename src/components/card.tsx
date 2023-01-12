14cbbdeec518f25f2a7d945a18ac8ac0d2af0e2e
Saiful Alam <saiful.rakib@miros-group.com>
2023-01-12 13:15:26 +0600

    Updated the homepage. [deploy]

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom"
 import { RightArrowIcon } from "./icons"
 
 export type CardProps = {
+    className?: string
     title: string
     description?: string
     link?: {
@@ -10,10 +11,10 @@ export type CardProps = {
     }
 }
 
-export function Card({ title, description, link }: CardProps) {
+export function Card({ title, description, link, className }: CardProps) {
 
     return (
-        <div className="max-w-sm w-64 p-6 bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+        <div className={"max-w-sm w-64 p-6 bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700 " + className}>
             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{title}</h5>
 
             {description && <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{description}</p>}
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,4 +1,4 @@
-import { Card, CardProps } from "src/components"
+import { Card, CardProps, Container } from "src/components"
 
 const PROJECTS: CardProps[] = [
     {
@@ -13,14 +13,14 @@ const PROJECTS: CardProps[] = [
 function Home() {
 
     return (
-        <div className="container mx-auto">
+        <Container className="flex flex-col">
             <h1 className="font-bold text-5xl my-10 text-center">Three JS Projects</h1>
             <div className='flex flex-wrap items-center my-4 justify-center m-4'>
                 {PROJECTS.map((item, index) => (
-                    <Card key={index} {...item} />
+                    <Card key={index} {...item} className="m-1" />
                 ))}
             </div>
-        </div>
+        </Container>
     )
 }
 
